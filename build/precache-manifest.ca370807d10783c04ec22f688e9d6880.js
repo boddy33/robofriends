df72da078475572c3ba272f3e6eead910b3e26f5
Bogdan <bogdan.kharchykov@gmail.com>
2020-09-01 21:20:51 +0200

    commit /public to try with git pages

---
+++ build/precache-manifest.ca370807d10783c04ec22f688e9d6880.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6214b77b8e797a405cda56f5f6aac9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "67e55ae7901e19f9835c",
+    "url": "/static/css/main.6bec35c0.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e55ae7901e19f9835c",
+    "url": "/static/js/main.f95026a5.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
